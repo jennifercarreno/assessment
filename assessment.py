f9419c69a3b362ded7f155e638ec7427de608c48
Jennifer Carreno <jennifercarreno72@gmail.com>
2021-10-05 10:52:17 -0700

    mvp

--- assessment.py
+++ assessment.py
@@ -1,7 +1,5 @@
 data_txt = "data.txt"
 infile = open(data_txt, "r")
-line1=infile.readline(0)
-print(line1)
 
 for line in infile:
     if "aman" in line:
@@ -12,9 +10,40 @@ for line in infile:
         print(beth)
     elif "camcam" in line:
         cam = line.split(',')
-        print(cam)
+        print(cam)        
 
+username_input = input('Enter Username: ')
+password_input = input('Enter Password: ')
 
+def display(username):
+    # print(username)
+    if username == andy["username"]:
+        load_andy_data(password_input)
+    if username == beth["username"]:
+        load_beth_data(password_input)
+    if username == cam["username"]:
+        load_cam_data(password_input)
+
+def load_andy_data(password):
+    if password == andy["password"]:
+        print("Name:", andy["name"])
+        print("Balance:", andy["balance"])
+    else: 
+        print("Username and Password not found")
+
+def load_beth_data(password):
+    if password == beth["password"]:
+        print("Name:", beth["name"])
+        print("Balance:", beth["balance"])
+    else: 
+        print("Username and Password not found")
+
+def load_cam_data(password):
+    if password == cam["password"]:
+        print("Name:", cam["name"])
+        print("Balance:", cam["balance"])
+    else: 
+        print("Username and Password not found")
 
 andy = {
     "username" : andy[0],
@@ -37,6 +66,6 @@ cam = {
     "balance" : cam[3]
 }
 
-
+display(username_input)
 
 infile.close()
